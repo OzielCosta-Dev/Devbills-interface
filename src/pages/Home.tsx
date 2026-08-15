b8674ea131f3538df349619d6c80dfac7ade9a18
OzielCosta-Dev <oziel19932016@outlook.com>
2026-08-15 15:51:51 -0300

    first-commit-devbills-interface

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Button from "../components/Button";
+
+const Home = () => {
+	return (
+		<div>
+			<h2>Home</h2>
+			<Button variant="outline" isLoading>Clique em Mim</Button>
+		</div>
+	);
+};
+
+export default Home;
