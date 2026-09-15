bb949d56aef1eff67527b4a539251271020f3dd2
OzielCosta-Dev <oziel19932016@outlook.com>
2026-09-14 22:36:57 -0300

    implementando-componente-card

---
+++ src/components/Card.tsx
@@ -0,0 +1,35 @@
+import type { ReactNode } from "react";
+
+interface CardProp {
+    children: ReactNode; 
+    title?: string;
+    subtitle?: String;
+    icon?: ReactNode;
+    hover:?: boolean;
+    glowEffect?: boolean;
+    className?: string;
+}
+
+
+
+
+
+const Card = ({ children, className = "", glowEffect = false, hover = false, icon, subtitle, title }: CardProps) => {
+
+
+
+    return (
+        <div className={`bg-gray-900 rounded-xl border border-gray-700 shadow-md p-6 transition-all *:**:
+                  ${hover ? "hover: border-primary-500 shadow-lg hover:-translate-y-0.5" : ""}
+
+
+        
+        `}>
+            
+        </div>
+    )
+}
+
+
+
+export default Card;
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,21 +1,31 @@
 import { useEffect, useState } from "react";
 import MonthYearSelect from "../components/MonthYearSelect";
 import { getTransactions } from "../services/transactionService";
+import type { TransactionSummary } from "../types/transactions";
+import Card from "../components/Card";
+
+const initialSumary: TransactionSummary = {
+	balance: 0,
+	totalExpenses: 0,
+	totalIncomes: 0,
+	expensesByCategory: [],
+};
 
 const Dashboard = () => {
 	const currentDate = new Date();
 	const [year, setYear] = useState<number>(currentDate.getFullYear());
 	const [month, setMonth] = useState(currentDate.getMonth() + 1);
+	const [summary, setSummary] = useState<TransactionSummary>(initialSumary);
 
 	useEffect(() => {
-		async function getTransactionsUser() {
-			const response = await getTransactions();
+		async function loadTransactionsSummary() {
+			const response = await getTransactionsSummary(month, year);
 
-			console.log(response);
+			setSummary(response);
 		}
 
-		getTransactionsUser();
-	}, []);
+		loadTransactionsSummary();
+	}, [month, year]);
 
 	return (
 		<div className="container-app py-6">
@@ -28,6 +38,7 @@ const Dashboard = () => {
 					onYearChange={setYear}
 				/>
 			</div>
+			<Card />
 		</div>
 	);
 };
